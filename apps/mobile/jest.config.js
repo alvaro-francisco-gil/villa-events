119c8e41e30ced8d91f959178857b8b2ec178aa8
Álvaro Francisco Gil <alvaro.francisco.gil@gmail.com>
2026-05-21 09:31:49 +0200

    test(mobile): wire jest-expo + RNTL setup and root mobile:test script

---
+++ apps/mobile/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'jest-expo',
+  setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
+  // pnpm stores packages under .pnpm/; include it so nested ESM packages get transpiled
+  transformIgnorePatterns: [
+    '/node_modules/(?!(.pnpm|react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@cultuvilla|nativewind))',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  collectCoverageFrom: ['lib/**/*.ts', 'components/**/*.{ts,tsx}'],
+};
